ff058bf27835e7192a3dd524b5a4ede2b1fc5091
Nathan Ridley <axefrog@gmail.com>
2025-06-08 23:13:43 +1000

    Added a union `Dynamic.Source` type for use with nested sources

--- source/dynamic/sources/index.ts
+++ source/dynamic/sources/index.ts
@@ -7,3 +7,25 @@ export * from './value-source/boolean-source';
 export * from './value-source/number-source';
 export * from './value-source/string-source';
 export * from './value-source/value-source';
+
+import { ArraySource } from './array/array-source';
+import { AssociativeRecordSource } from './associative-record/associative-record-source';
+import { FixedRecordSource } from './fixed-record/fixed-record-source';
+import { MapSource } from './map-source/map-source';
+import { SetSource } from './set-source/set-source';
+import { BooleanSource } from './value-source/boolean-source';
+import { NumberSource } from './value-source/number-source';
+import { StringSource } from './value-source/string-source';
+import { ValueSource } from './value-source/value-source';
+
+export type Source =
+  | ArraySource<any>
+  | AssociativeRecordSource<any>
+  | FixedRecordSource<any>
+  | MapSource<any, any>
+  | SetSource<any>
+  | BooleanSource
+  | NumberSource
+  | StringSource
+  | ValueSource
+;
