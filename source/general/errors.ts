7659cb808d0f0bbaf547872d4d788b528b231ac5
Nathan Ridley <axefrog@gmail.com>
2025-07-16 17:59:44 +1000

    Added a facility for capturing runtime diagnostic/logging details

--- source/general/errors.ts
+++ source/general/errors.ts
@@ -1,4 +1,5 @@
-import { isString } from './type-checking';
+import { Detail } from './detail';
+import { isNumber, isString } from './type-checking';
 
 export const throwError = (error: Error | string): never => { throw isString(error) ? new Error(error) : error; };
 export const ThrowError = (error: Error | string): (...args: any[]) => never => () => throwError(error);
@@ -18,3 +19,20 @@ export class ThisShouldBeUnreachable extends Error {
 }
 
 export class DeliberateTermination extends Error {}
+
+export class DetailedError<TType extends string | number = string | number, TRefs = unknown> extends Error {
+  constructor (type: TType, message: string, refs?: TRefs);
+  constructor (arg: Detail<TType, TRefs> | Detail.Args<TType, TRefs>);
+  constructor (arg0: Detail<TType, TRefs> | Detail.Args<TType, TRefs> | TType, message?: string, refs?: TRefs) {
+    const details = arg0 instanceof Detail ? arg0
+      : isString(arg0) || isNumber(arg0) ? new Detail({ type: arg0, description: message!, refs: refs! })
+      : new Detail(arg0);
+    super();
+    this.cause = details;
+    this.details = details;
+  }
+  public readonly details: Detail<TType, TRefs>;
+
+  get type (): TType { return this.details.type; }
+  get message (): string { return this.details.description; }
+}
